b9998de7c41f301298f8eb318274c195ba75db8d
sergeistetsko <sergeistetsko2021@yandex.by>
2024-06-28 19:15:34 +0300

    add new task

---
+++ chapter2/task3/index.js
@@ -0,0 +1,20 @@
+(function(){
+    let msg = 'JavaScript'; msg += ' Fun'
+    console.log('Add & concatenate: ' + msg)
+
+    let sum = 5.00; sum += 2.50
+    console.log('Add & assign decimal: ' + sum)
+
+    sum = 8; sum -= 4
+    console.log('Subtract & assign integer: ' + sum)
+
+    sum = 8; sum *= 4
+    console.log('Multiply & assign integer: ' + sum)
+
+    sum = 8; sum /= 4
+    console.log('Divide & assign integer: ' + sum)
+
+    sum = 8; sum %= 4
+    console.log('Modulus & assign integer: ' + sum)
+    
+})()

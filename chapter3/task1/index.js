8d2a123b6af3d9341c4579299a19265321330aef
sergeistetsko <sergeistetsko2021@yandex.by>
2024-07-02 15:22:48 +0300

    add new task

---
+++ chapter3/task1/index.js
@@ -0,0 +1,13 @@
+let flag = true
+if (!flag) {
+    console.log('Power is OFF')
+}
+if (flag) {
+    console.log('Power is ON')
+}
+if (7 < 2) {
+    console.log('Failure')
+}
+if (7 > 2) {
+    console.log('Success')
+}

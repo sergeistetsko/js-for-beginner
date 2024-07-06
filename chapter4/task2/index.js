99f913ab473f7217b509de2cbb88f7071c809f59
sergeistetsko <sergeistetsko2021@yandex.by>
2024-07-06 19:43:14 +0300

    add new task

---
+++ chapter4/task2/index.js
@@ -0,0 +1,29 @@
+(function() {
+    let car = {
+        make: 'Jeep',
+        model: 'Wrangler',
+        accelerate: function () {
+            return this.model + ' drives away'
+        },
+        brake: function () {
+            return this.make + ' pulls up'
+        },
+    }
+    for (property in car) {
+        console.log(property + ': ' + car[property])
+    } 
+    car.make = 'Ford'
+    car.model = 'Bronco'
+    car.engine = 'V6'
+    car.start = function () {
+        return this.make + ' motor is running'
+    }
+
+    console.log('\nCar is a ' + car.make + '' + car['model'])
+    console.log('Engine Type: ' + car.engine)
+
+    console.log(car.start())
+    console.log(car.accelerate())
+    console.log(car.brake())
+
+}) ()

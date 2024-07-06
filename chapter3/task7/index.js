a874a7aab3eb47dc3e57916cb3b111e083d6ec5b
sergeistetsko <sergeistetsko2021@yandex.by>
2024-07-06 18:35:42 +0300

    add new task

---
+++ chapter3/task7/index.js
@@ -0,0 +1,19 @@
+(function() {
+let i = 0
+let j = 0
+
+for (i = 1; i < 3; i++) {
+    console.log('Outer Loop: ' + i)
+    for (j = 1; j < 4; j++) {
+        if ((i === 1) && (j === 2)) {
+            console.log('\tSkipped: ' + j)
+            continue
+        }
+        if ((i === 2) && (j === 2)) {
+            console.log('\tBroken: ' + j)
+            break
+        }
+        console.log('\tInner Loop: ' + j)
+    }
+}
+}) ()
